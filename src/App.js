87458e398934ab1f1d11f75fa69f087ca1dc30e8
Jal Panchal <jal08panchal@gmail.com>
2019-05-04 06:22:42 -0700

    Importing Header in app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Header from "./components/Header";
 import Contact from "./components/Contact";
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <div className="App">
       <h1> Contact Mananger</h1>
       <Contact />
+      <Header />
     </div>
   );
 }
