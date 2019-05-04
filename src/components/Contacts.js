4ed044d3ba4bbbff68b409fa1fd1588ef5c4683f
Jal Panchal <jal08panchal@gmail.com>
2019-05-04 10:29:28 -0700

    Creating state for contacts

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,6 +1,13 @@
 import React, { Component } from "react";
 
 class Contacts extends Component {
+  state = {
+    contacts: [
+      { id: 1, name: "Jal", email: "jal@gmail.com", phone: 555 - 555 - 555 },
+      { id: 2, name: "Avadhya", email: "avj@gmail.com", phone: 1111111 },
+      { id: 3, name: "Avj", email: "avj@gmail.com", phone: 1111111 }
+    ]
+  };
   render() {
     return <div />;
   }
