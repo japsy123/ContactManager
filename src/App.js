9507b1fcccee571b5d2fa406dd8b8139ca11fde7
Jal Panchal <jal08panchal@gmail.com>
2019-05-04 08:32:35 -0700

    Importing bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./components/Header";
 import Contact from "./components/Contact";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
