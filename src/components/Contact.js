3048c413345c715bfb924534cd461624c3b2c65d
Jal Panchal <jal08panchal@gmail.com>
2019-05-04 09:46:08 -0700

    Creating Contacts component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,7 +5,7 @@ class Contact extends Component {
   render() {
     const { name, email, phone } = this.props;
     return (
-      <div className="card card-body ">
+      <div className="card card-body mb-3">
         <h4> {name}</h4>
         <ul className="list-group">
           <li className="list-group-item">{email}</li>
